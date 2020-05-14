0399ab12f1e8e655b7b6e8d2f41a294381bacbd7
mertoz41 <mertozkaynak@windowslive.com>
2020-05-14 15:51:04 -0400

    checkbox added on parts

--- app/controllers/machine_controller.rb
+++ app/controllers/machine_controller.rb
@@ -8,6 +8,26 @@ class MachineController < Sinatra::Base
         erb :index
     end
 
+    get "/machines/new" do
+        @parts = Part.all 
+        erb :new
+    end 
+
+    post "/machines" do
+        prod = nil
+        if  /true/i.match?(params[:in_production])
+            prod = true
+        else
+            prod = false
+        end
+        machine = Machine.find_or_create_by(name: params[:name], machine_type: params[:machine_type], brand: params[:brand], in_production: prod)
+        params[:parts][0].each do |keys, id|
+            # binding.pry
+            MachinePart.create(machine_id: machine.id, part_id: id)
+        end
+        redirect "/machines/#{machine.id}"
+    end 
+
     get "/machines/:id" do
 
         @mach = Machine.find(params[:id])
--- app/controllers/part_controller.rb
+++ app/controllers/part_controller.rb
@@ -8,11 +8,26 @@ class PartController < Sinatra::Base
         erb :index
     end
 
+    get "/parts/new" do 
+        erb :new
+    end 
+
+    post "/parts" do
+      part = Part.find_or_create_by(name: params[:name], brand: params[:brand], reference_number: params[:reference_number])
+        redirect "/parts/#{part.id}"
+    end 
+
     get "/parts/:id" do
 
         @part = Part.find(params[:id])
 
         erb :show
+    end
+
+    delete "/parts/:id" do
+        part_id = params[:id]
+        Part.destroy(part_id)
+        redirect "/parts"
     end
 
     
